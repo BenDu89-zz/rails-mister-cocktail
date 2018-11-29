c745c988eea5e8090578d74caad969f82e9e4736
Benjamin Dunisch <bdunisch@googlemail.com>
2018-11-29 17:01:53 +0000

    added the de:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+require 'net/http'
+require 'json'
+require 'byebug'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +9,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "startet"
+Ingredient.delete_all
+puts "deleted all "
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+answer = JSON.parse(response)
+  answer["drinks"].each do |ans|
+    Ingredient.create(name: ans['strIngredient1'])
+  end
+puts 'Finished!'
